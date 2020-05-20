229f090e421400c0448c7b0437c3082011e40222
Diego Barneche <diego.barneche@ymail.com>
2020-05-20 12:56:14 +0800

    updates to data documentation

--- R/data.R
+++ R/data.R
@@ -64,9 +64,16 @@
 #' @docType data
 NULL
 
-#' Water Bodies
+#' Marine Water Bodies within the Great Barrier Reef
 #'
-#' Description
+#' The GBRMPA delineation as published in the guidelines are: Enclosed
+#' Coastal (Queensland Water Quality Guidelines); Open Coastal (AIMS
+#' description of coastal water body relative distance from Research
+#' Publication no. 89); Midshelf (AIMS description of inner shelf water
+#' body relative distance from Research Publication no. 89); and Offshsore
+#' (AIMS description of relative distance from Research Publication
+#' no. 89) Coral Sea taken from DEWHA definition of current Coral Sea
+#' Conservation Park.
 #' 
 #' Data kindly provided by:
 #' Great Barrier Reef Marine Park Authority
@@ -117,15 +124,16 @@ NULL
 #' @docType data
 NULL
 
-#' Name of file
+#' Great Barrier Reef Marine Park Boundary
 #'
-#' Lines of general description
-#' Lines of general description
-#' Lines of general description
+#' Derived from the Amalgamated Great Barrier Reef (AGBR) Section that
+#' replaces all previously existing  sections.The AGBR was proclaimed
+#' under subsection 31 (1) of the Act in 2003 (Gazette No S119) dated 21
+#' April 2004.
 #' 
 #' For full Metadata see the original
-#' GBRMPA Geoportal (add link)
-#' page info for the Data X.
+#' \href{https://tinyurl.com/y93w8euw}{GBRMPA Geoportal}
+#' page info for the Great Barrier Reef Marine Park Boundary.
 #'
 #' Data kindly provided by:
 #' Great Barrier Reef Marine Park Authority
@@ -153,24 +161,58 @@ NULL
 #' }
 #' 
 #' @format A \href{https://cran.r-project.org/package=sf}{Simple Features data.frame}
-#' with XX rows and XX variables:
+#' with 1 row and 13 variables:
 #' \itemize{
-#'   \item 1:
-#'   \item 2:
+#'   \item OBJECTID:
+#'   \item GIS_AREA:
+#'   \item sdc_extern:
+#'   \item HECTARES:
+#'   \item UNIQUE_ID:
+#'   \item LOC_TYPE_S:
+#'   \item LOC_NAME_S:
+#'   \item LOC_NAM_L:
+#'   \item SHAPE_Leng:
+#'   \item GlobalID:
+#'   \item Shape_area:
+#'   \item Shape_len:
+#'   \item geometry:
 #' }
 #' @name gbr_bounds
 #' @docType data
 NULL
 
-#' Name of file
+#' Great Barrier Reef Marine Park Zoning
 #'
-#' Lines of general description
-#' Lines of general description
-#' Lines of general description
+#' The Great Barrier Reef Marine Park Act 1975 (the Act) provides for the establishment, 
+#' control, care and development of the Great Barrier Reef Marine Park (the Marine Park). 
+#' The Great Barrier Reef Marine Park Authority (the Authority) is responsible for the 
+#' management of the Marine Park. The Great Barrier Reef Marine Park Zoning Plan 2003 is 
+#' the primary planning instrument for the conservation and management of the Marine Park. 
+#' In having regard to the objects set out in subsection 32 (7) of the Act, this Zoning Plan 
+#' takes account of the world heritage values of the Marine Park and the principles of 
+#' ecologically sustainable developments. This Zoning Plan aims, in conjunction with other 
+#' management mechanisms, to protect and conserve the biodiversity of the Great Barrier 
+#' Reef ecosystem within a network of highly protected zones, while providing opportunities 
+#' for the ecologically sustainable use of, and access to, the Great Barrier Reef Region by 
+#' current and future generations. In addition to the protection of representative areas of 
+#' biodiversity, this Zoning Plan also provides for the protection of other areas of high 
+#' conservation value by assigning protective zoning to a range of habitats such as coral reefs, 
+#' sponge beds, seagrass beds and deep water areas, as well as important dugong habitats and 
+#' other special or unique sites. The Marine Park is managed as a multiple use area. This means 
+#' that, while enhancing the conservation of the Marine Park, this Zoning Plan also provides for 
+#' a range of recreational, commercial and research opportunities, and for the continuation of 
+#' traditional activities. This Zoning Plan expressly acknowledges the rights and interests of 
+#' indigenous Australians in the Marine Park by providing for the management of the traditional 
+#' use of marine resources, including traditional hunting, in accordance with Aboriginal and Torres 
+#' Strait Islander custom and tradition. The contributions of scientific research to the management 
+#' and understanding of the Marine Park are also acknowledged in this Zoning Plan, which provides 
+#' for the management of research in the Marine Park, including a system of Scientific Research Zones 
+#' to facilitate research around scientific research stations.(Extract from "Preface A, Introduction; 
+#' Great Barrier Reef Marine Park Zoning Plan 2003).
 #' 
 #' For full Metadata see the original
-#' GBRMPA Geoportal (add link)
-#' page info for the Data X.
+#' \href{https://tinyurl.com/y7l3ysu6}{GBRMPA Geoportal}
+#' page info for the Great Barrier Reef Marine Park Zoning.
 #'
 #' Data kindly provided by:
 #' Great Barrier Reef Marine Park Authority
@@ -198,24 +240,56 @@ NULL
 #' }
 #' 
 #' @format A \href{https://cran.r-project.org/package=sf}{Simple Features data.frame}
-#' with XX rows and XX variables:
+#' with 756 rows and 26 variables:
 #' \itemize{
-#'   \item 1:
-#'   \item 2:
+#'   \item OBJECTID:
+#'   \item NAME:
+#'   \item IUCN:
+#'   \item NRS_PA:
+#'   \item GAZ_AREA:
+#'   \item GIS_AREA:
+#'   \item RES_NO:
+#'   \item GAZ_DATE:
+#'   \item LATEST_GAZ:
+#'   \item STATE:
+#'   \item AUTHORITY:
+#'   \item ENVIRON:
+#'   \item CODE:
+#'   \item X_COORD:
+#'   \item Y_COORD:
+#'   \item ALT_ZONE:
+#'   \item TYPE:
+#'   \item AREA_TYPE:
+#'   \item LOC_NAME_S:
+#'   \item PERMIT_DES:
+#'   \item GLOBALID:
+#'   \item GROUP_ID:
+#'   \item STD_PERMIT:
+#'   \item Shape_area:
+#'   \item Shape_len:
+#'   \item geometry:
 #' }
 #' @name gbr_zoning
 #' @docType data
 NULL
 
-#' Name of file
+#' Natural Resource Management Marine Regions Relating to the 
+#' Great Barrier Reef Marine Park (Version 2)
 #'
-#' Lines of general description
-#' Lines of general description
-#' Lines of general description
+#' This dataset was created to provide a maritime extension of the largely
+#' terrestrial Natural Resource Management Regions. It will be used as a
+#' planning tool when assessing water quality issues adjacent to the
+#' Natural Resource Management Regions, within or adjacent to the Great
+#' Barrier Reef Marine Park.
+#' 
+#' A marine extension of the Natural Resource Management Regions, covering 
+#' an area from the Queensland mainland at low water, excluding all islands 
+#' and cays.
 #' 
 #' For full Metadata see the original
-#' GBRMPA Geoportal (add link)
-#' page info for the Data X.
+#' \href{https://tinyurl.com/yb3lgqf4}{eAtlas}
+#' page info for the NRM areas flowing into the 
+#' Great Barrier Reef Marine Park.
 #'
 #' Data kindly provided by:
 #' Great Barrier Reef Marine Park Authority
@@ -243,20 +317,31 @@ NULL
 #' }
 #' 
 #' @format A \href{https://cran.r-project.org/package=sf}{Simple Features data.frame}
-#' with XX rows and XX variables:
+#' with 6 rows and 9 variables:
 #' \itemize{
-#'   \item 1:
-#'   \item 2:
+#'   \item OBJECTID:
+#'   \item NAME:
+#'   \item NRM_BODY:
+#'   \item COMMENTS:
+#'   \item IQATLAS_QL:
+#'   \item Area_KM2:
+#'   \item Shape_STAr:
+#'   \item Shape_STLe:
+#'   \item geometry:
 #' }
 #' @name nrm_regions
 #' @docType data
 NULL
 
-#' Name of file
+#' Hydrology data from GEODATA TOPO 250K Series 3
 #'
-#' Lines of general description
-#' Lines of general description
-#' Lines of general description
+#' This is a subset of the original GEODATA TOPO 250K Series 3 dataset
+#' made available by Geoscience Australia. The original
+#' GEODATA TOPO 250K Series 3 is a vector representation of the major 
+#' topographic features appearing on the 1:250,000 scale NATMAPs supplied 
+#' in Shape file format and is designed for use in a range of commercial 
+#' GIS software. Data is arranged within specific themes. All data is 
+#' based on the GDA94 coordinate system.
 #' 
 #' For full Metadata see the original
 #' Geoscience Australia Data Portal
@@ -297,10 +382,25 @@ NULL
 #' }
 #' 
 #' @format A \href{https://cran.r-project.org/package=sf}{Simple Features data.frame}
-#' with XX rows and XX variables:
+#' with 496,036 rows and 17 variables:
 #' \itemize{
-#'   \item 1:
-#'   \item 2:
+#'   \item OBJECTID:
+#'   \item FEATTYPE:
+#'   \item TYPE:
+#'   \item NAME:
+#'   \item PERENNIAL:
+#'   \item HIERARCHY:
+#'   \item FEATREL:
+#'   \item ATTRREL:
+#'   \item PLANACC:
+#'   \item SOURCE:
+#'   \item CREATED:
+#'   \item RETIRED:
+#'   \item PID:
+#'   \item SYMBOL:
+#'   \item TEXTNOTE:
+#'   \item SHAPE_Leng:
+#'   \item geometry:
 #' }
 #' @name qld_hydro
 #' @docType data
